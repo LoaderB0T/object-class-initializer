73703a10be88912b8fac778571636fcd2a03209f
Janik Schumacher <rndmb0t@gmail.com>
2024-02-16 12:53:06 +0100

    renamed function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 type ClassToMapped<T> = { [K in keyof T]: T[K] } & {};
 
-export function build<T, M extends T>(c: new () => M): ClassToMapped<M>;
-export function build<T>(c: new () => T): T;
-export function build<T>(c: new () => T): T {
+export function createObject<T, M extends T>(c: new () => M): ClassToMapped<M>;
+export function createObject<T>(c: new () => T): T;
+export function createObject<T>(c: new () => T): T {
   return Object.assign({}, new c());
 }
