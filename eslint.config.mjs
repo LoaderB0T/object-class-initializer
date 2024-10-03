8276d247b74f61a36ea4e99a2e3c4eabd3e982d6
Janik Schumacher <rndmb0t@gmail.com>
2024-10-04 01:37:06 +0200

    linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,22 +4,8 @@ export default [
   {
     ignores: ['test/**/*', '**/jest.config.ts', 'eslint.config.mjs'],
   },
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ['*.js', '*.mjs'],
-          defaultProject: true,
-        },
-        tsconfigRootDir: import.meta.name,
-        allowDefaultProject: true,
-      },
-    },
-  },
   ...awdwareNode,
   {
-    rules: {
-      'switch-colon-spacing': 'warn',
-    },
+    rules: {},
   },
 ];
